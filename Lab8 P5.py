981ae4f30e5217f35975dc0c0601b9d705b4d0d4
czacarias905 <60229233+czacarias905@users.noreply.github.com>
2020-03-08 19:39:18 -0500

    Add files via upload

---
+++ Lab8 P5.py
@@ -0,0 +1,22 @@
+#Cecilia Zacarias
+#3/5/2020
+
+#Problem 5 is a function that checks weather a character has picked up all the items needed to perform certian tasks and checks against any status debuffs
+
+#Really try on this one dont understand debuffing 
+game_items = ["pan","paper","idea","rope","groceries"]
+
+debuffs: [slow]
+
+def task_1:
+    game_items.append("coat","first aid kit")
+    for task_1 in game_items
+
+
+
+def task_2:
+
+
+
+def tsk_3:
+    game_items.append("pen")
